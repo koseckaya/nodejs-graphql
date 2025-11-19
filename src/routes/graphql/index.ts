ea245b7dfd5166511b8da6c63cc043d699fd9c0b
MilaKoseckaya <kosetsaya@gmail.com>
2025-11-19 22:41:16 +0200

    add query depth validation to prevent abuse

--- src/routes/graphql/index.ts
+++ src/routes/graphql/index.ts
@@ -1,7 +1,8 @@
 import { FastifyPluginAsyncTypebox } from '@fastify/type-provider-typebox';
 import { createGqlResponseSchema, gqlResponseSchema } from './schemas.js';
-import { graphql } from 'graphql';
+import { graphql, parse, validate } from 'graphql';
 import { schema } from './entities/schema.js';
+import depthLimit from 'graphql-depth-limit';
 
 const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
   const { prisma } = fastify;
@@ -16,6 +17,15 @@ const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
     },
     async handler(req) {
       const { query, variables } = req.body;
+
+      const validationResults = validate(schema, parse(query), [depthLimit(5)]);
+
+      if (validationResults.length) {
+        return {
+          errors: validationResults,
+        };
+      }
+
       return graphql({
         schema,
         source: query,
